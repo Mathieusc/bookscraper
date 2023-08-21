a6f7f8b877f05f75b65ec0b6faa2d8497165ddc7
Mathieusc <mathieu.schweitzer@otmail.fr>
2023-08-22 00:38:30 +0200

    BookScraper class layout

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,9 @@
-import aiohttp
 import asyncio
+from scraper import BookScraper
 
+async def main():
+    book_url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+    scraper = BookScraper(book_url)
+    await scraper.fetch_book_data()
+
+asyncio.run(main())
--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,3 +1,19 @@
-class Scraper:
-    def __init__(self):
-        pass
+from bs4 import BeautifulSoup
+import aiohttp
+import asyncio
+import csv
+
+
+class BookScraper:
+    def __init__(self, book_url):
+        self.book_url = book_url
+        self.book_data = {}
+
+    async def fetch_book_data(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.book_url) as response:
+                html = await response.text()
+                soup = BeautifulSoup(html, "html.parser")
+                # Find the title for example for one book
+                print(soup.find("h1").text)
+                print(soup)
