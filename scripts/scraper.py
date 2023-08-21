d6d9468c10e1d5f716f05e2a714281a989cf60c0
Mathieusc <mathieu.schweitzer@otmail.fr>
2023-08-22 01:11:16 +0200

    extract_product_page_url method

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -16,4 +16,55 @@ class BookScraper:
                 soup = BeautifulSoup(html, "html.parser")
                 # Find the title for example for one book
                 print(soup.find("h1").text)
+                
                 print(soup)
+
+                test = self.extract_universal_product_code(soup)
+                print(test)
+
+        return soup
+
+    def extract_product_page_url(self, soup):
+        """ADD THIS AT THE END"""
+        product_url = ""
+        return product_url
+    
+    def extract_universal_product_code(self, soup):
+        upc = soup.find("th", text="UPC").find_next_sibling()
+        return upc.text
+    
+    def extract_title(self, soup):
+        title = ""
+        return title
+    
+    def extract_price_including_tax(self, soup):
+        price_with_tax = ""
+        return price_with_tax
+    
+    def extract_price_excluding_tax(self, soup):
+        price_without_tax = ""
+        return price_without_tax
+    
+    def extract_number_available(self, soup):
+        number_available = ""
+        return number_available
+    
+    def extract_product_description(self, soup):
+        description = ""
+        return description
+    
+    def extract_category(self, soup):
+        category = ""
+        return category
+    
+    def extract_review_rating(self, soup):
+        rating = ""
+        return rating
+    
+    def extract_image_url(self, soup):
+        image_url = ""
+        return image_url
+    
+
+    
+
